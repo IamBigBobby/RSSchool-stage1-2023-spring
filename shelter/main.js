69987ad13ffc79c350e514a44eb9a66a3d9cbc20
Denis Svetleishii <denissvetleishii@outlook.com>
2023-04-05 18:52:59 +0400

    feat: add burger

---
+++ shelter/main.js
@@ -0,0 +1,49 @@
+// burger
+
+let burger = document.querySelector('.burger_menu')
+console.log(burger)
+let navigationPanel = document.querySelector('.list_of_navigation')
+let transparentBackground = document.querySelector('.transparent_background_none')
+
+function moveBurger(){
+    if (navigationPanel.classList.contains('list_of_navigation')){
+        navigationPanel.classList.remove('list_of_navigation');
+        navigationPanel.classList.add('list_of_navigation_show');
+
+        burger.classList.remove('burger_menu');
+        burger.classList.add('burger_menu_rotate');
+
+        transparentBackground.classList.remove('transparent_background_none');
+        transparentBackground.classList.add('transparent_background_show');
+
+        document.body.style.overflow = 'hidden';
+    }
+    else{
+        navigationPanel.classList.remove('list_of_navigation_show');
+        navigationPanel.classList.add('list_of_navigation');
+
+        burger.classList.remove('burger_menu_rotate');
+        burger.classList.add('burger_menu');
+
+        transparentBackground.classList.remove('transparent_background_show');
+        transparentBackground.classList.add('transparent_background_none');
+
+        document.body.style.overflow = 'visible';
+    }
+}
+
+function closeBurgerByBackground(){
+    navigationPanel.classList.remove('list_of_navigation_show');
+    navigationPanel.classList.add('list_of_navigation');
+
+    burger.classList.remove('burger_menu_rotate');
+    burger.classList.add('burger_menu');
+
+    transparentBackground.classList.remove('transparent_background_show');
+    transparentBackground.classList.add('transparent_background_none');
+
+    document.body.style.overflow = 'visible';
+}
+
+burger.addEventListener('click', moveBurger);
+transparentBackground.addEventListener('click', closeBurgerByBackground)
