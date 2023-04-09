f2cf0ab11682e0344a5bcfcb2527dddb97f0b02f
Denis Svetleishii <denissvetleishii@outlook.com>
2023-04-09 21:41:34 +0400

    feat: add final slider

--- shelter/main.js
+++ shelter/main.js
@@ -1,3 +1,96 @@
+const listOfDogs = `[
+    {
+      "name": "Jennifer",
+      "img": "assets/png/pets-jennifer.png",
+      "type": "Dog",
+      "breed": "Labrador",
+      "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+      "age": "2 months",
+      "inoculations": ["none"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Sophia",
+      "img": "assets/png/pets-sophia.png",
+      "type": "Dog",
+      "breed": "Shih tzu",
+      "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+      "age": "1 month",
+      "inoculations": ["parvovirus"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Woody",
+      "img": "assets/png/pets-woody.png",
+      "type": "Dog",
+      "breed": "Golden Retriever",
+      "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
+      "age": "3 years 6 months",
+      "inoculations": ["adenovirus", "distemper"],
+      "diseases": ["right back leg mobility reduced"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Scarlett",
+      "img": "assets/png/pets-scarlet.png",
+      "type": "Dog",
+      "breed": "Jack Russell Terrier",
+      "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
+      "age": "3 months",
+      "inoculations": ["parainfluenza"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Katrine",
+      "img": "assets/png/pets-katrine.png",
+      "type": "Cat",
+      "breed": "British Shorthair",
+      "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
+      "age": "6 months",
+      "inoculations": ["panleukopenia"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Timmy",
+      "img": "assets/png/pets-timmy.png",
+      "type": "Cat",
+      "breed": "British Shorthair",
+      "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
+      "age": "2 years 3 months",
+      "inoculations": ["calicivirus", "viral rhinotracheitis"],
+      "diseases": ["kidney stones"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Freddie",
+      "img": "assets/png/pets-freddie.png",
+      "type": "Cat",
+      "breed": "British Shorthair",
+      "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
+      "age": "2 months",
+      "inoculations": ["rabies"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Charly",
+      "img": "assets/png/pets-charly.png",
+      "type": "Dog",
+      "breed": "Jack Russell Terrier",
+      "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
+      "age": "8 years",
+      "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
+      "diseases": ["deafness", "blindness"],
+      "parasites": ["lice", "fleas"]
+    }
+]`
+
+const parsedData = JSON.parse(listOfDogs);
+
 // burger
 
 let burger = document.querySelector('.burger_menu');
@@ -53,13 +146,27 @@ link.forEach(element => element.addEventListener('click', closeBurgerByBackgroun
 
 // slider
 
+
+
+let sliderLeft = document.querySelector('.circle_left');
+let sliderRight = document.querySelector('.circle_right');
+let listOfPets = document.querySelectorAll('.card_friend');
+
+sliderRight.addEventListener('click', function (){
+    listOfPets.forEach(element => element.classList.remove('card_friend'));
+    changeToBackward()
+    forward();
+})
+sliderLeft.addEventListener('click', function(){
+    changeToForward();
+    backward();
+})
+
 let pastArr = [];
 let currArr = [];
 let nextArr = [];
-const range = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const range = parsedData;
 let arrLength = 0;
-
-
 
 function checkWindow(){
     if (window.innerWidth > 1290){
@@ -81,7 +188,7 @@ checkWindow();
 function init(){
     randomRangeArr = range.sort(() => (Math.random() - 0.5));
 
-    
+    console.log(randomRangeArr)
     for (let i = 0; i <= arrLength; i++){
         nextArr.push(randomRangeArr[i]);
     }
@@ -100,36 +207,46 @@ function init(){
 
 
     for (let i = 0; i <= arrLength; i++){
-        nextArr.push(randomRangeArr[arrLength * 2 + 2 + i]);
+        nextArr.push(randomRangeArr[arrLength * 2 + 1 + i]);
     }
 
+
+    pushToHtml();
 }
 init();
 
 function forward(){
 
-    console.log(nextArr);
-    console.log(currArr);
-    console.log(pastArr);
 
     pastArr.splice(0, pastArr.length)
+
+
+
     pastArr.push(...currArr);
+
+
     currArr.splice(0, currArr.length);
+
+
+
     currArr.push(...nextArr);
+
+
     nextArr.splice(0, nextArr.length);
     
     
     randomRangeArr = range.sort(() => (Math.random() - 0.5));
 
     for (let i = 0; i <= arrLength; i++){
-        nextArr.push(randomRangeArr[i]);
+        nextArr.push(randomRangeArr[arrLength * 2 + i]);
     }
 
-    console.log(nextArr);
-    console.log(currArr);
-    console.log(pastArr);
+    let removePets = document.querySelectorAll('.card_friend');
+    removePets.forEach(element => element.remove());
+    
+
+    pushToHtml();
 }
-// forward();
 
 function backward(){
 
@@ -146,8 +263,12 @@ function backward(){
         pastArr.push(randomRangeArr[i]);
     }
 
+    let removePets = document.querySelectorAll('.card_friend');
+    removePets.forEach(element => element.remove());
+    
+
+    pushToHtml();
 }
-backward();
 
 
 
@@ -172,8 +293,12 @@ function changeToBackward(){
     for (let i = 0; i <= arrLength; i++){
         nextArr.push(randomRangeArr[i]);
     }
+
+    let removePets = document.querySelectorAll('.card_friend');
+    removePets.forEach(element => element.remove());
+
+    pushToHtml();
 }
-// changeToBackward()
 
 function changeToForward(){
     let replaceArrCurr = [];
@@ -186,41 +311,118 @@ function changeToForward(){
     nextArr.splice(0, nextArr.length)
 
 
-    currArr.push(...replaceArrPast);
-    pastArr.push(...replaceArrCurr);
+    currArr.push(...replaceArrNext);
+    nextArr.push(...replaceArrCurr);
 
-    nextArr.splice(0, nextArr.length);
+    pastArr.splice(0, nextArr.length);
 
     randomRangeArr = range.sort(() => (Math.random() - 0.5));
 
     for (let i = 0; i <= arrLength; i++){
-        nextArr.push(randomRangeArr[i]);
+        pastArr.push(randomRangeArr[i]);
+    }
+
+    let removePets = document.querySelectorAll('.card_friend');
+    removePets.forEach(element => element.remove());
+
+    pushToHtml();
+}
+// changeToForward();
+
+function pushToHtml(){
+    const containerOfPets = document.querySelector('.our_friends_about');
+
+    let imgPet = '';
+    let namePet = '';
+    for (let pet of currArr){
+        namePet = pet.name;
+        imgPet = pet.img
+        
+        let html = `
+        <div class="card_friend">
+            <img class="photo_of_friend" src="${imgPet}" alt="pet katrine">
+            <p class="name_of_friend">${namePet}</p>
+            <button class="light_button button_learn_more">Learn more</button>
+        </div>
+    `;
+    containerOfPets.insertAdjacentHTML('afterbegin', html);
     }
 }
-changeToForward();
 
 
 
 
-// console.log(nextArr)
 
 
 
-// function generateNextArr(){
-//     min = Math.ceil(0);
-//     max = Math.floor(7);
 
-//     for (let i = 0; i < arrLength; i++){
-//         pushNumber = Math.floor(Math.random() * (max - min + 1)) + min;       
-//     }
-//     return nextArr;
-// }
-// console.log(generateNextArr());
 
-// function compareCurrentAndNext (){
-//     console.log(nextArr.sort());
-//     console.log(currArr.sort());
-//     let sortNextArr = nextArr.sort();
-//     let sortCurrArr = currArr.sort();
-// }
-// compareCurrentAndNext();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
