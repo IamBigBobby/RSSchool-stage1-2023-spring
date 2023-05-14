a4105fcb6cfa67ea725eaf5c554a92d0f02bbdd6
Denis Svetleishii <denissvetleishii@outlook.com>
2023-05-14 15:50:53 +0400

    feat: add table field

--- minesweeper/src/utils/createFiled.js
+++ minesweeper/src/utils/createFiled.js
@@ -1,9 +1,19 @@
 export function createField(){
-  let mineFiled = [];
+  let mineFiledArr = [];
 
   for (let i = 0; i < 10; i++){
-    mineFiled[i] = Math.round(Math.random() * 99999999).toString(2).slice(1,11).split('');
+    mineFiledArr[i] = Math.round(Math.random() * 99999999).toString(2).slice(1,11).split('');
   }
   
-  console.log(mineFiled);
+  console.log(mineFiledArr);
+
+  let minesweeperProject = document.querySelector('.minesweeper-project');
+  minesweeperProject.innerHTML = `<table class="mineField"></table>`;
+
+  minesweeperProject.insertAdjacentHTML('afterbegin', html);
+
+  // for (let i = 0; i < 10; i++){
+  //   let td = document.createElement('td');
+
+  // }
 }
