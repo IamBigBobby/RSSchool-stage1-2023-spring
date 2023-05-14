cc68b87dc869f79c156f27fae1e86c0fb68b8957
Denis Svetleishii <denissvetleishii@outlook.com>
2023-05-14 17:50:22 +0400

    fix: rework logic of drop bombs

--- minesweeper/src/utils/createMatrix.js
+++ minesweeper/src/utils/createMatrix.js
@@ -1,9 +1,16 @@
 export let mineFiledArr = [];
+let countBomb = 10;
 
 export function createMatrix (){
 
-  for (let i = 0; i < 10; i++){
-    mineFiledArr[i] = Math.round(Math.random()).toString(2).slice(1,11);
+  for (let i = 0; i < 100; i++){
+    mineFiledArr[i] = 0;
+  }
+
+
+  for (let i = 0; i < countBomb; i++){
+    let randomNumberArr = Math.floor(Math.random() * mineFiledArr.length);
+    mineFiledArr.splice(randomNumberArr, 1, 1);
   }
   console.log(mineFiledArr)
 }
