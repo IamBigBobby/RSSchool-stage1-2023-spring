16073c3bb2ddaf15a2026c9b52fee68ba438dce4
Denis Svetleishii <denissvetleishii@outlook.com>
2023-05-21 14:03:51 +0400

    feat: add flags

--- minesweeper/src/utils/gameLogic.js
+++ minesweeper/src/utils/gameLogic.js
@@ -3,22 +3,20 @@ import { mineField } from "./createFiled.js";
 import { twoDimensionalArr } from "./createMatrix.js";
 
 export function clickButton(event){
-  console.log(event);
+
   document.oncontextmenu = function(event){
     event.preventDefault();
   }
   if (event.button === 0){
-    console.log('left');
+    checkField(event.target.x, event.target.y);
   }
   else if (event.button === 2){
     console.log('right');
+    addFlag(event);
   }
 }
 
-export function сheckField(x, y){
-
-  x = this.x;
-  y = this.y;
+function checkField(x, y){
 
   if (twoDimensionalArr[y][x] === 1){
     console.log('game over');
@@ -32,7 +30,7 @@ function checkAroundCells(x, y){
   if (x > 9 || y > 9 || x < 0 || y < 0){
     return;
   }
-  if (mineField.rows[y].cells[x].classList.contains('empty') || mineField.rows[y].cells[x].classList.contains('near-bomb')){
+  if (mineField.rows[y].cells[x].classList.contains('empty') || mineField.rows[y].cells[x].classList.contains('near-bomb') || mineField.rows[y].cells[x].classList.contains('flag')){
     return;
   }
   let numberOfBombs = 0;
@@ -73,8 +71,6 @@ function checkAroundCells(x, y){
     return;
   }
   else{
-    console.log(x);
-    console.log(y);
     mineField.rows[y].cells[x].classList.add('empty');
     if (y >= 0 && y <= 9 && x >=0 && x <= 9){
       checkAroundCells(x + 1, y);
@@ -90,7 +86,7 @@ function checkAroundCells(x, y){
 }
 
 function addFlag(event){
-  if(event.button === 2){
-    this.classList.add('green')
+  if ((event.target.classList.contains('clear') || event.target.classList.contains('bomb')) && !event.target.classList.contains('near-bomb') && !event.target.classList.contains('empty')){
+    event.target.classList.toggle('flag');
   }
 }
